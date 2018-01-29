48039a7b7c8811793e775412641a3d5989dd5a61
Maria_Pang <530912338@qq.com>
2018-01-29 18:04:34 +0800

    添加排序逻辑

--- MLTools/MLTools/ViewController.m
+++ MLTools/MLTools/ViewController.m
@@ -27,15 +27,29 @@
     [super viewDidLoad];
     self.view.backgroundColor = COLOR(252, 157, 154, 1.0);
     
-    NSArray * items = @[@"问题优化",@"bug", @"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug", @"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug",@"积分相关",@"信息安全问题",@"提现困难", @"如何使用积分帮助",@"问题优化",@"bug"];
+    NSArray * items = @[@"问题优化",@"angel", @"积分相关",@"信息安全问题",@"提现困难",@"问题优化",@"quit",@"如何使用积分帮助",@"积分相关",@"信息安全问题",@"别难过",@"去旅行",@"hug",@"dog",@"除了你还有谁",@"IP",@"曼丽", @"哦",@"庞",@"google",@"just",@"离开",@"第一次排序",@"你在哪个城市", @"恩非常不错",@"我喜欢旅游呀",@"peak",@"反正我喜欢你", @"如何使用积分帮助",@"患难见真情",@"eagle",@"看过来",@"kit",@"积分相关",@"谷歌一下你就知道",@"爱你一万年",@"facebook",@"积分相关",@"杰伦变丑了",@"little",@"nut",@"站在巨人肩膀上", @"圆满",@"嘻嘻",@"matter",@"傻不傻",@"ok",@"bug"];
     
-    MLAutoLaySelectLabelsView * selectView = [[MLAutoLaySelectLabelsView alloc]initWithFrame:CGRectMake(10, 50, SCREEN_WIDTH - 20, self.view.frame.size.height - 50 ) items:items];
+   //添加排序逻辑
+   NSArray * newArr = [items sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+       
+       return [obj1 localizedCompare:obj2];
+    }];
+    
+    for (NSString * str in newArr) {
+        
+        NSLog(@"----- %@", [str description]);
+    }
+
+    
+    MLAutoLaySelectLabelsView * selectView = [[MLAutoLaySelectLabelsView alloc]initWithFrame:CGRectMake(10, 50, SCREEN_WIDTH - 20, self.view.frame.size.height - 50 ) items:newArr];
 
 //    selectView.btnLabelMargin = GetLogicPixelX(100);
 //    selectView.gapX  = 20;
     
     [self.view addSubview:selectView];
 }
+
+
 
 
 
